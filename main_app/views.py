9f68b81d24ac9a495e96aebb0b545547319c6a70
pksnowstorm <enguyen21@gmail.com>
2023-04-27 10:00:10 -0500

    Create initial about

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
   return HttpResponse('<h1>Hello ૮ ・ﻌ・ა</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the DogCollector</h1>')
