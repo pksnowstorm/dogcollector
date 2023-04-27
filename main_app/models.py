2a9452914f1e0e2c1e8a2fef36381c56ec565461
pksnowstorm <enguyen21@gmail.com>
2023-04-27 13:57:21 -0500

    add and delete dog

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.urls import reverse
+
 # Create your models here.
 class Dog(models.Model):
     name = models.CharField(max_length=100)
@@ -8,3 +10,5 @@ class Dog(models.Model):
     age = models.IntegerField()
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 # Create your views here.
 from django.http import HttpResponse
@@ -17,3 +18,16 @@ def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     return render(request, 'dogs/detail.html', { 'dog': dog })
 
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/'
+
+class DogUpdate(UpdateView):
+    model = Dog
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+    model = Dog
+    success_url = '/dogs/'
