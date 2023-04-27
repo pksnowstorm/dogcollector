2b582c1272a265220a045876ca62a83148132bc5
pksnowstorm <enguyen21@gmail.com>
2023-04-27 13:33:00 -0500

    create detail page

--- main_app/views.py
+++ main_app/views.py
@@ -13,3 +13,7 @@ def dogs_index(request):
     dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { 'dogs': dogs })
 
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog })
+
