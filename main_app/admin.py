4136254c723a11c4f5dd790c26d5217ae29fd24b
pksnowstorm <enguyen21@gmail.com>
2023-04-27 14:20:11 -0500

    add feeding

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Dog
+from .models import Dog, Feeding
 admin.site.register(Dog)
+admin.site.register(Feeding)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 # Create your views here.
 from django.http import HttpResponse
 
@@ -16,7 +17,19 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
-    return render(request, 'dogs/detail.html', { 'dog': dog })
+    feeding_form = FeedingForm()
+    return render(request, 'dogs/detail.html', { 'dog': dog, 'feeding_form': feeding_form })
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
 
 class DogCreate(CreateView):
     model = Dog
