880d3f67e41de97f130e5abe7762c513418405cf
pksnowstorm <enguyen21@gmail.com>
2023-04-28 05:37:18 -0500

    Add user and authorization

--- main_app/urls.py
+++ main_app/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('toys/<int:pk>/', views.ToysDetail.as_view(), name='toys_detail'),
     path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
     path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Dog, Toy
 from .forms import FeedingForm
 # Create your views here.
@@ -12,8 +16,9 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def dogs_index(request):
-    dogs = Dog.objects.all()
+    dogs = Dog.objects.filter(user=request.user)
     return render(request, 'dogs/index.html', { 'dogs': dogs })
 
 def dogs_detail(request, dog_id):
@@ -25,10 +30,26 @@ def dogs_detail(request, dog_id):
     'toys': toys_dog_doesnt_have 
     })
 
-class DogCreate(CreateView):
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+class DogCreate(LoginRequiredMixin, CreateView):
     model = Dog
     fields = ['name', 'breed', 'description', 'age']
-    success_url = '/dogs/'
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class DogUpdate(UpdateView):
     model = Dog
